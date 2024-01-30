d696958d8275aae359f11b8462beac96212a9c84
ssh <ss830513@gmail.com>
2024-01-30 13:30:59 +0800

    Add First Ajax

--- Msit155site/Controllers/ApiController.cs
+++ Msit155site/Controllers/ApiController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Msit155site.Models;
+using System.Text;
 
 namespace Msit155site.Controllers
 {
@@ -11,15 +12,29 @@ namespace Msit155site.Controllers
             _context = context;
         }
 
-
+        
         public IActionResult Index()
         {
-            return View();
+            return Content("<H1>Content 中文</H1>","text/plain",Encoding.UTF8);
         }
+        [HttpPost]
         public IActionResult Cities()
         {
             var Cities = _context.Addresses.Select(p => p.City).Distinct();
             return Json(Cities);
+        }
+        public IActionResult Avatar(int id = 1)
+        {
+            Member? member = _context.Members.Find(id);
+            if(member != null)
+            {
+                byte[] img = member.FileData;
+                if (img != null)
+                {
+                    return File(img, "image/jpeg");
+                }
+            }
+            return NotFound();
         }
     }
 }
--- Msit155site/Controllers/HomeController.cs
+++ Msit155site/Controllers/HomeController.cs
@@ -17,7 +17,10 @@ namespace Msit155site.Controllers
         {
             return View();
         }
-
+        public IActionResult First()
+        {
+            return View();
+        }
         public IActionResult Privacy()
         {
             return View();
